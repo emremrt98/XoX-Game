6d4fe1810489c8c6ac88620921fd0a4783e3517a
Emre Mert <emre.mrt98@gmail.com>
2022-08-25 21:47:34 +0300

    v3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,74 @@
 import Xox from './xox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './main.css'
+import { Alert } from 'bootstrap';
 
 function App() {
   const [state, setState] = useState(true);
-  const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
 
+  const [control, setControl] = useState({
+    a0: "",
+    a1: "",
+    a2: "",
+    a3: "",
+    a4: "",
+    a5: "",
+    a6: "",
+    a7: "",
+    a8: ""
+  })
 
+  useEffect(() => {
+    const { a0, a1, a2, a3, a4, a5, a6, a7, a8 } = control;
 
+    const winner = (state ? "O" : "X")
+    const win1 = (a0 === 'X' && a1 === 'X' && a2 === 'X') || (a0 === 'O' && a1 === 'O' && a2 === 'O');
+    const win2 = (a3 === 'X' && a4 === 'X' && a5 === 'X') || (a3 === 'O' && a4 === 'O' && a5 === 'O');
+    const win3 = (a6 === 'X' && a7 === 'X' && a8 === 'X') || (a6 === 'O' && a7 === 'O' && a8 === 'O');
+    const win4 = (a0 === 'X' && a4 === 'X' && a8 === 'X') || (a0 === 'O' && a4 === 'O' && a8 === 'O');
+    const win5 = (a6 === 'X' && a4 === 'X' && a2 === 'X') || (a6 === 'O' && a4 === 'O' && a2 === 'O');
+    const win6 = (a6 === 'X' && a3 === 'X' && a0 === 'X') || (a6 === 'O' && a3 === 'O' && a0 === 'O');
+    const win7 = (a1 === 'X' && a4 === 'X' && a7 === 'X') || (a1 === 'O' && a4 === 'O' && a7 === 'O');
+    const win8 = (a2 === 'X' && a5 === 'X' && a8 === 'X') || (a2 === 'O' && a5 === 'O' && a8 === 'O')
+    if (win1) {
+      alert(`Kazanan ${winner}`)
+    } else if (win2) {
+      alert(`Kazanan ${winner}`)
+    } else if (win3) {
+      alert(`Kazanan ${winner}`)
+    } else if (win4) {
+      alert(`Kazanan ${winner}`)
+    } else if (win5) {
+      alert(`Kazanan ${winner}`)
+    } else if (win6) {
+      alert(`Kazanan ${winner}`)
+    } else if (win7) {
+      alert(`Kazanan ${winner}`)
+    } else if (win8) {
+      alert(`Kazanan ${winner}`)
+    }
+  }, [control])
   return (
 
-    < div >
-      <div className="App">
-        <div>
-          <Xox value={0} state={state} setState={setState} />
-          <Xox value={1} state={state} setState={setState} />
-          <Xox value={2} state={state} setState={setState} />
-        </div>
-        <div>
-          <Xox value={3} state={state} setState={setState} />
-          <Xox value={4} state={state} setState={setState} />
-          <Xox value={5} state={state} setState={setState} />
-        </div>
-        <div>
-          <Xox value={6} state={state} setState={setState} />
-          <Xox value={7} state={state} setState={setState} />
-          <Xox value={8} state={state} setState={setState} />
-        </div>
+    <div className='App'>
+
+      <div>
+        <Xox setControl={setControl} value={"a0"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a3"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a6"} state={state} setState={setState} />
       </div>
+      <div>
+        <Xox setControl={setControl} value={"a1"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a4"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a7"} state={state} setState={setState} />
+      </div>
+      <div>
+        <Xox setControl={setControl} value={"a2"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a5"} state={state} setState={setState} />
+        <Xox setControl={setControl} value={"a8"} state={state} setState={setState} />
+      </div>
+
+
 
     </div >
   );
--- src/xox.js
+++ src/xox.js
@@ -1,25 +1,36 @@
 import { useState } from "react"
 
-export default function Xox({ state, setState, value, }) {
+export default function Xox({ state, setState, value, setControl }) {
     const [word, setWord] = useState("");
     const [color, setColor] = useState("");
-    const update = () => {
+    const update = (value) => {
+
         if (state) {
             setWord("X");
             setColor("x")
+            setControl(prevList => ({ ...prevList, [value]: "X" }))
         }
         else {
             setWord("O");
             setColor("o")
+            setControl(prevList => ({ ...prevList, [value]: "O" }))
         }
 
         setState(!state)
+
     }
 
 
     return (
         <div>
-            <button onClick={update} className={color} type="button">{word}</button>
+            <button onClick={() => {
+                update(value);
+
+            }}
+                className={color}
+                type="button">
+                {word}
+            </button>
         </div>
     )
 }
