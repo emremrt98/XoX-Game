bc51d3bd7a6c16075985fa6a1445c0cddcfeee4b
Emre Mert <emre.mrt98@gmail.com>
2022-08-27 00:23:39 +0300

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Xox from './xox';
 import { useEffect, useState } from 'react';
 import './main.css'
-import { Alert } from 'bootstrap';
+
 
 function App() {
   const [state, setState] = useState(true);
@@ -17,55 +17,80 @@ function App() {
     a7: "",
     a8: ""
   })
+  const [res, setRes] = useState("");
+  const result = () => {
+    if (state === true) {
+      setRes("Kazanan O");
+    } else {
+      setRes("Kazanan X")
+    }
+  }
 
   useEffect(() => {
     const { a0, a1, a2, a3, a4, a5, a6, a7, a8 } = control;
+    let loseOrWin = false;
+ 
 
-    const winner = (state ? "O" : "X")
-    const win1 = (a0 === 'X' && a1 === 'X' && a2 === 'X') || (a0 === 'O' && a1 === 'O' && a2 === 'O');
-    const win2 = (a3 === 'X' && a4 === 'X' && a5 === 'X') || (a3 === 'O' && a4 === 'O' && a5 === 'O');
-    const win3 = (a6 === 'X' && a7 === 'X' && a8 === 'X') || (a6 === 'O' && a7 === 'O' && a8 === 'O');
-    const win4 = (a0 === 'X' && a4 === 'X' && a8 === 'X') || (a0 === 'O' && a4 === 'O' && a8 === 'O');
-    const win5 = (a6 === 'X' && a4 === 'X' && a2 === 'X') || (a6 === 'O' && a4 === 'O' && a2 === 'O');
-    const win6 = (a6 === 'X' && a3 === 'X' && a0 === 'X') || (a6 === 'O' && a3 === 'O' && a0 === 'O');
-    const win7 = (a1 === 'X' && a4 === 'X' && a7 === 'X') || (a1 === 'O' && a4 === 'O' && a7 === 'O');
-    const win8 = (a2 === 'X' && a5 === 'X' && a8 === 'X') || (a2 === 'O' && a5 === 'O' && a8 === 'O')
-    if (win1) {
-      alert(`Kazanan ${winner}`)
-    } else if (win2) {
-      alert(`Kazanan ${winner}`)
-    } else if (win3) {
-      alert(`Kazanan ${winner}`)
-    } else if (win4) {
-      alert(`Kazanan ${winner}`)
-    } else if (win5) {
-      alert(`Kazanan ${winner}`)
-    } else if (win6) {
-      alert(`Kazanan ${winner}`)
-    } else if (win7) {
-      alert(`Kazanan ${winner}`)
-    } else if (win8) {
-      alert(`Kazanan ${winner}`)
+    if ((a0 === 'X' && a1 === 'X' && a2 === 'X') || (a0 === 'O' && a1 === 'O' && a2 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a3 === 'X' && a4 === 'X' && a5 === 'X') || (a3 === 'O' && a4 === 'O' && a5 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a6 === 'X' && a7 === 'X' && a8 === 'X') || (a6 === 'O' && a7 === 'O' && a8 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a0 === 'X' && a4 === 'X' && a8 === 'X') || (a0 === 'O' && a4 === 'O' && a8 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a6 === 'X' && a4 === 'X' && a2 === 'X') || (a6 === 'O' && a4 === 'O' && a2 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a6 === 'X' && a3 === 'X' && a0 === 'X') || (a6 === 'O' && a3 === 'O' && a0 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a1 === 'X' && a4 === 'X' && a7 === 'X') || (a1 === 'O' && a4 === 'O' && a7 === 'O')) {
+      loseOrWin = true;
+      result();
+      
+    } else if ((a2 === 'X' && a5 === 'X' && a8 === 'X') || (a2 === 'O' && a5 === 'O' && a8 === 'O')) {
+      loseOrWin = true;
+      result();
+      
     }
+
+    if (a0 !== '' && a1 !== '' && a2 !== '' && a3 !== '' && a4 !== '' && a5 !== '' && a6 !== '' && a7 !== '' && a8 !== '' && state === false) {
+
+      setRes("Berabere")
+    }
+
   }, [control])
   return (
 
-    <div className='App'>
-
-      <div>
-        <Xox setControl={setControl} value={"a0"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a3"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a6"} state={state} setState={setState} />
-      </div>
-      <div>
-        <Xox setControl={setControl} value={"a1"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a4"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a7"} state={state} setState={setState} />
-      </div>
-      <div>
-        <Xox setControl={setControl} value={"a2"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a5"} state={state} setState={setState} />
-        <Xox setControl={setControl} value={"a8"} state={state} setState={setState} />
+    <div >
+      <h1 style={{ textAlign: "center" }}>{res}</h1>
+      <div className='App'>
+        <div>
+          <Xox setControl={setControl} value={"a0"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a3"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a6"} state={state} setState={setState} />
+        </div>
+        <div>
+          <Xox setControl={setControl} value={"a1"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a4"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a7"} state={state} setState={setState} />
+        </div>
+        <div>
+          <Xox setControl={setControl} value={"a2"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a5"} state={state} setState={setState} />
+          <Xox setControl={setControl} value={"a8"} state={state} setState={setState} />
+        </div>
       </div>
 
 
